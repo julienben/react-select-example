ba269cc81bd1473b94abdafdb1cd029874314e53
Julien Benchetrit <julienbenchetrit@gmail.com>
2018-11-12 14:09:39 +0200

    PostCSS support is no longer needed

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import')(),
-    require('precss')(),
-    // We need stage 2 for :not pseudo-class
-    require('postcss-preset-env')({ stage: 2 }),
-    require('postcss-ie11-pseudo-class'),
-  ],
-};
